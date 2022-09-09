9bd1abf34d8fe59f6d0d2e8a067870b209491d2b
David Wagner <wagdav@gmail.com>
2022-09-09 18:35:53 +0200

    simple working

--- src/main/net/thewagner/skiers/core.clj
+++ src/main/net/thewagner/skiers/core.clj
@@ -1,59 +1,65 @@
 ; Example from https://github.com/mbrooker/simulator_example/blob/main/ski_sim.py
 (ns net.thewagner.skiers.core
-  (:require [clojure.data.priority-map :refer [priority-map]]))
+  (:require [shams.priority-queue :as pq]))
 
-; Skier state :waiting :riding-lift :skiing
-(def skier {:entity :skier
-            :state :waiting})
+(defn dec0 [x]
+  (max (dec x) 0))
 
-(def lift {:entity :lift
-           :state :leaving})
+(defn clamp [x a b]
+  (cond
+    (< x a)    a
+    (<= a x b) x
+    (< b x)    b))
 
-(defmulti handle-event (fn [_state _time e] (:event e)))
+(defmulti handle-event (fn [state event] (:event event)))
 
-(defmethod handle-event :default [state t event] state)
+(defmethod handle-event :default [state event] state)
 
 ; Skier
-(defmethod handle-event :skier/waiting [state t event]
-  (let [skiing-time 5]
-    (update state :lift-queue conj (:skier/id event))))
+(defmethod handle-event :skier/joined-queue [state event]
+  (-> state
+    (update :skiers/skiing dec0)
+    (update :skiers/waiting inc)))
 
-(defmethod handle-event :skier/skiing [state t event]
-  (let [skiing-time 5]
-    (update state :events conj [(assoc event :event :skier/waiting) (+ t skiing-time)])))
+(defmethod handle-event :skier/left-lift [state event]
+  (let [skiing-time 50]
+    (-> state
+      (update :skiers/riding-lift dec0)
+      (update :skiers/skiing inc)
+      (update :events conj {:event :skier/joined-queue :t (+ (event :t) skiing-time)}))))
 
 ; Lift
-(defmethod handle-event :lift/leaving [state t event]
-  (let [lift-time 1
-        lift-capacity 4
-        skiers (take lift-capacity (:lift-queue state))]
+(defmethod handle-event :lift/leaving [state event]
+  (let [ride-time 10
+        chair-width 4
+        chair-period 4
+        riders (clamp (:skiers/waiting state) 0 chair-width)]
     (-> state
-      (update :events into (for [s skiers] [(assoc s :event :skier/skiing) (+ t lift-time)]))
-      (update :events conj [event (+ t lift-time lift-time)])
-      (update :lift-queue #(drop lift-capacity %)))))
+      (update :skiers/waiting - riders)
+      (update :skiers/riding-lift + riders)
+      (update :events into (replicate riders {:event :skier/left-lift :t (+ (event :t) ride-time)}))
+      (update :events conj {:event :lift/leaving :t (+ (event :t) chair-period)}))))
+
+(def initial-state
+  {:skiers/waiting 2
+   :skiers/riding-lift 0
+   :skiers/skiing 0
+   :events (pq/priority-queue (fn [e] (- (:t e))) :elements [{:event :lift/leaving :t 0}])})
 
 (defn simulate []
   (let [end-time 20]
     (loop [t 0
-           state {:lift-queue (clojure.lang.PersistentQueue/EMPTY)
-                  :skiers {:a {:state :waiting}
-                           :b {:state :waiting}}
-                  :events (priority-map {:event :skier/waiting :skier/id :a} 0
-                                        {:event :skier/waiting :skier/id :b} 0
-                                        {:event :lift/leaving} 0)}]
+           state initial-state]
       (let [{:keys [events]} state]
         (if (and (seq events) (<= t end-time))
-          (let [[ev t] (peek events)
-                new-state (handle-event (update state :events pop) t ev)]
-            (println t ev state)
-            (recur t new-state))
+          (let [ev (peek events)
+                new-state (handle-event (update state :events pop) ev)]
+            (println ev state)
+            (recur (ev :t) new-state))
           state)))))
 
 (comment
-  (handle-event {:events []} 10 {:event :skier/waiting :skier/id "Bob"})
-  (handle-event {} 0 {:event :skier/skiing :skier/id "Alice"})
-  (handle-event {:lift-queue [{:skier 1} {:skier 2}]
-                 :events []}
-                0
-                {:entity :lift :state :leaving})
+  (handle-event initial-state {:event :skier/joined-queue :t 0})
+  (handle-event initial-state {:event :lift/leaving :t 0})
+  (handle-event initial-state {:event :skier/left-lift :t 0})
   (simulate))
