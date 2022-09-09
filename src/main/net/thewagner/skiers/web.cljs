8fac6171ec72ccc83953a24678075c38659ba0c1
David Wagner <wagdav@gmail.com>
2022-09-09 18:35:53 +0200

    Put everything in an atom

--- src/main/net/thewagner/skiers/web.cljs
+++ src/main/net/thewagner/skiers/web.cljs
@@ -9,25 +9,26 @@
 
 (goog-define ^string revision "main")
 
-(def vlSpec
-  {:$schema "https://vega.github.io/schema/vega-lite/v5.json"
-   :description "A simple bar chart with embedded data."
-   :data {:values [{:a "A"  :b 28}
-                   {:a "B"  :b 55}
-                   {:a "C"  :b 43}
-                   {:a "D"  :b 91}
-                   {:a "E"  :b 81}
-                   {:a "F"  :b 53}
-                   {:a "G"  :b 19}
-                   {:a "H"  :b 87}
-                   {:a "I"  :b 52}]}
-   :mark "bar"
-   :encoding {:x {:field "a"
-                  :type "ordinal"
-                  :title "a-value"}
-              :y {:field "b"
-                  :type "quantitative"
-                  :title "b-value"}}})
+(defonce vlSpec
+  (r/atom
+    {:$schema "https://vega.github.io/schema/vega-lite/v5.json"
+     :description "A simple bar chart with embedded data."
+     :data {:values [{:a "A"  :b 28}
+                     {:a "B"  :b 55}
+                     {:a "C"  :b 43}
+                     {:a "D"  :b 91}
+                     {:a "E"  :b 81}
+                     {:a "F"  :b 53}
+                     {:a "G"  :b 19}
+                     {:a "H"  :b 87}
+                     {:a "I"  :b 52}]}
+     :mark "bar"
+     :encoding {:x {:field "a"
+                    :type "ordinal"
+                    :title "a-value"}
+                :y {:field "b"
+                    :type "quantitative"
+                    :title "b-value"}}}))
 
 (defn main []
   [:<>
@@ -37,7 +38,7 @@
         [:div.column
           "Graph on the right"]
         [:div.column
-          [:> react-vega/VegaLite {:spec vlSpec}]]]]])
+          [:> react-vega/VegaLite {:spec @vlSpec}]]]]])
 
 (defn mount []
   (rdom/render [main] (gdom/getElement "app")))
@@ -52,5 +53,6 @@
   (require '[shadow.cljs.devtools.api :as shadow])
   (shadow/repl :app)
   ; Exit the CLJS session
-  :cljs/quit)
+  :cljs/quit
 
+  (swap! vlSpec update-in [:data :values 5 :b] + 10))
