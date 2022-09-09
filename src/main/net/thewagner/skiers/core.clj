3a94a061717ac4b4170c1789de55594691fdb298
David Wagner <wagdav@gmail.com>
2022-09-09 18:35:53 +0200

    Simplify?

--- src/main/net/thewagner/skiers/core.clj
+++ src/main/net/thewagner/skiers/core.clj
@@ -9,26 +9,26 @@
 (def lift {:entity :lift
            :state :leaving})
 
-(defmulti handle-event (fn [_state _time e] ((juxt :entity :state) e)))
+(defmulti handle-event (fn [_state _time e] (:event e)))
 
 (defmethod handle-event :default [state t event] state)
 
 ; Skier
-(defmethod handle-event [:skier :waiting] [state t event]
+(defmethod handle-event :skier/waiting [state t event]
   (let [skiing-time 5]
-    (update state :lift-queue conj event)))
+    (update state :lift-queue conj (:skier/id event))))
 
-(defmethod handle-event [:skier :skiing] [state t event]
+(defmethod handle-event :skier/skiing [state t event]
   (let [skiing-time 5]
-    (update state :events conj [(assoc event :state :waiting) (+ t skiing-time)])))
+    (update state :events conj [(assoc event :event :skier/waiting) (+ t skiing-time)])))
 
 ; Lift
-(defmethod handle-event [:lift :leaving] [state t event]
+(defmethod handle-event :lift/leaving [state t event]
   (let [lift-time 1
         lift-capacity 4
         skiers (take lift-capacity (:lift-queue state))]
     (-> state
-      (update :events into (for [s skiers] [(assoc s :state :skiing) (+ t lift-time)]))
+      (update :events into (for [s skiers] [(assoc s :event :skier/skiing) (+ t lift-time)]))
       (update :events conj [event (+ t lift-time lift-time)])
       (update :lift-queue #(drop lift-capacity %)))))
 
@@ -36,9 +36,11 @@
   (let [end-time 20]
     (loop [t 0
            state {:lift-queue (clojure.lang.PersistentQueue/EMPTY)
-                  :events (priority-map {:entity :skier :id 1 :state :waiting} 0
-                                        {:entity :skier :id 2 :state :waiting} 0
-                                        {:entity :lift :state :leaving} 0)}]
+                  :skiers {:a {:state :waiting}
+                           :b {:state :waiting}}
+                  :events (priority-map {:event :skier/waiting :skier/id :a} 0
+                                        {:event :skier/waiting :skier/id :b} 0
+                                        {:event :lift/leaving} 0)}]
       (let [{:keys [events]} state]
         (if (and (seq events) (<= t end-time))
           (let [[ev t] (peek events)
@@ -48,8 +50,8 @@
           state)))))
 
 (comment
-  (handle-event {:events []} 10 skier)
-  (handle-event {} 0 (merge skier {:state :skiing}))
+  (handle-event {:events []} 10 {:event :skier/waiting :skier/id "Bob"})
+  (handle-event {} 0 {:event :skier/skiing :skier/id "Alice"})
   (handle-event {:lift-queue [{:skier 1} {:skier 2}]
                  :events []}
                 0
