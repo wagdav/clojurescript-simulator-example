fd0e38767521a810616a2e21196d410d8bf68a59
David Wagner <wagdav@gmail.com>
2022-09-09 18:35:53 +0200

    Move everythig in a big state

--- src/main/net/thewagner/skiers/core.clj
+++ src/main/net/thewagner/skiers/core.clj
@@ -3,29 +3,38 @@
   (:require [clojure.data.priority-map :refer [priority-map]]))
 
 ; Skier state :waiting :riding-lift :skiing
-(def skier {:state :waiting})
+(def skier {:entity :skier
+            :state :waiting})
 
-(defn handle [t {state :state :as event}]
-  (println "@" t event)
-  (let [skiing-time 5
-        waiting-time 10]
-    (case state
-      :waiting [(assoc event :state :skiing) (+ t waiting-time)]
-      :skiing  [(assoc event :state :waiting) (+ t skiing-time)])))
+(def lift {:entity :lift
+           :state :leaving})
+
+(defmulti handle-event (fn [_state _time e] ((juxt :entity :state) e)))
+
+(defmethod handle-event [:skier :waiting] [state t event]
+  (let [skiing-time 5]
+    (update state :events conj [(assoc event :state :waiting) (+ t skiing-time)])))
+
+(defmethod handle-event [:skier :skiing] [state t event]
+  (let [skiing-time 5]
+    (update state :events conj [(assoc event :state :waiting) (+ t skiing-time)])))
+
+(defmethod handle-event :lift [_ t event]
+  (let [lift-time 10]))
 
 (defn simulate []
   (let [end-time 20]
     (loop [t 0
-           events (priority-map {:skier 1 :state :waiting} t)]
-      (if (and (seq events) (<= t end-time))
-        (let [[ev t] (peek events)
-              new-events (partition 2 (handle t ev))]
-          (recur t (if (seq new-events)
-                     (into (pop events) new-events)
-                     (pop events))))
-        events))))
+           state {:lift-queue (clojure.lang.PersistentQueue/EMPTY)
+                  :events (priority-map {:entity :skier :id 1 :state :waiting} 0)}]
+      (let [{:keys [events]} state]
+        (if (and (seq events) (<= t end-time))
+          (let [[ev t] (peek events)
+                new-state (handle-event (update state :events pop) t ev)]
+            (recur t new-state))
+          state)))))
 
 (comment
-  (handle 0 {:state :waiting})
-  (handle 0 {:state :skiing})
+  (handle-event {:events []} 10 skier)
+  (handle-event {} 0 (merge skier {:state :skiing}))
   (simulate))
