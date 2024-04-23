4feef773e2df3a0322ae89ab3ece98efe51f0171
David Wagner <wagdav@gmail.com>
2024-04-23 09:20:01 +0200

    Update to nixpkgs 23.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Numerical simulator in ClojureScript";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-compat = {
       url = github:edolstra/flake-compat;
       flake = false;
